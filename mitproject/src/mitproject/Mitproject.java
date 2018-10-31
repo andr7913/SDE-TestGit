5a34c73d2c8ccf0a988837ac081549ae4b4e2dc9
andreas <andreas-cooper@hotmail.com>
2018-10-31 13:27:21 +0100

    Update mitproject/src/mitproject/Mitproject.java

--- mitproject/src/mitproject/Mitproject.java
+++ mitproject/src/mitproject/Mitproject.java
@@ -17,19 +17,16 @@ public class Mitproject
      */
     public static void main(String[] args)
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         Robot r= new Robot();
         System.out.println("robot:" + r);
-=======
+
         robot r= new robot();
         //System.out.println();
->>>>>>> parent of 69a46bd... lavet sout
-=======
+
         robot r= new robot();
         //System.out.println();
->>>>>>> parent of 69a46bd... lavet sout
-        // TODO code application logic here
+
     }
     
 }
